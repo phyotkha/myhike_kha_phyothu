7ec63e76c41d967f5fc3f5dd81cc4e26f01b3f9c
Phyo Thu Kha <phyothukha.ptk@outlook.com>
2024-02-20 10:22:05 -0800

    Did some demo

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -1,14 +1,15 @@
 //----------------------------------------
 //  Your web app's Firebase configuration
 //----------------------------------------
-var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyCizc9YF_T08TWu8yA-TXPSSLj6WN04MKk",
+    authDomain: "comp1800-demo-23c61.firebaseapp.com",
+    projectId: "comp1800-demo-23c61",
+    storageBucket: "comp1800-demo-23c61.appspot.com",
+    messagingSenderId: "188145797795",
+    appId: "1:188145797795:web:99794f76bf83afb7f7cd16"
+  };
+  
 
 //--------------------------------------------
 // initialize the Firebase app
